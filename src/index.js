53fc4f52c99e073641acd0f8370a7e01766cfe2f
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-06 04:33:03 +0400

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let result = 0;
+  n = Math.abs(n);
+  while (n > 0) {
+    result *= 10;
+    result += (n % 10);
+    n -= (n % 10);
+    n /= 10;
+  }
+  return result;
 };
