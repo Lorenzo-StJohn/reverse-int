a65836cb2721758d8c5abe197f049c15418fdeea
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-06 04:35:04 +0400

    fix: get rid of linter error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function reverse(n) {
   let result = 0;
-  n = Math.abs(n);
-  while (n > 0) {
+  let currentN = Math.abs(n);
+  while (currentN > 0) {
     result *= 10;
-    result += (n % 10);
-    n -= (n % 10);
-    n /= 10;
+    result += currentN % 10;
+    currentN -= currentN % 10;
+    currentN /= 10;
   }
   return result;
 };
